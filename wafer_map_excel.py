847fe2603b43a42a60e0ce1f0f4581bacba2c0fb
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2022-01-25 16:23:43 +0800

    Update wafer_map_excel.py

--- wafer_map_excel.py
+++ wafer_map_excel.py
@@ -33,59 +33,53 @@ file_dir = f'C:/Users/gohja/Desktop/wafer-map-automation/{filename}'
 wb = load_workbook(filename=filename)
 ws = wb.active
 
-# Create a scatter chart
-chart = ScatterChart()
-chart.title = "Scatter Chart Automation Test"
-chart.legend = None
+# # Create a scatter chart
+# chart = ScatterChart()
+# chart.title = "Scatter Chart Automation Test"
+# chart.legend = None
 
-x_col = column_index_from_string(to_plot_columns[0])
-y_col = column_index_from_string(to_plot_columns[1])
-min_row = to_plot_rows[0]
-max_row = to_plot_rows[1]
+# x_col = column_index_from_string(to_plot_columns[0])
+# y_col = column_index_from_string(to_plot_columns[1])
+# min_row = to_plot_rows[0]
+# max_row = to_plot_rows[1]
 
-xvalues = Reference(ws, min_col=x_col, min_row=min_row, max_row=max_row)
-yvalues = Reference(ws, min_col=y_col, min_row=min_row, max_row=max_row)
+# xvalues = Reference(ws, min_col=x_col, min_row=min_row, max_row=max_row)
+# yvalues = Reference(ws, min_col=y_col, min_row=min_row, max_row=max_row)
 
-# Plot the points on the scatter chart
-series = Series(xvalues, yvalues)
-# chart.add_data(xvalues, yvalues)
+# # Plot the points on the scatter chart
+# series = Series(xvalues, yvalues)
+# series.marker = Marker('circle', size=15)
+# series.graphicalProperties.line.noFill = True
 
-# series.marker = DataPoint(
-#     idx=5, spPr=GraphicalProperties(solidFill="800000"))
-
-series.marker = Marker('circle', size=15)
-series.graphicalProperties.line.noFill = True
-# s.marker.graphicalProperties.solidFill = "800000"
-
-pt = DataPoint(idx=10)
-pt.marker = Marker('circle', size=15)
-pt.graphicalProperties.line.noFill = True
-
-# pt = DataPoint(idx=10,
-#                marker=Marker('circle', size=15),
-#                spPr=GraphicalProperties(solidFill="800000"))
-
-pt = DataPoint(idx=5)
-pt.graphicalProperties.solidFill = "800000"
-
-series.dPt.append(pt)
-
-chart.series.append(series)
-
-ws.add_chart(chart, "G14")
+# chart.series.append(series)
+# ws.add_chart(chart, "G14")
 
 #
 # Calculation of the area fraction, and coloring the markers on the chart
 #
 
-# area_fraction_rows = area_fraction['rows']
-# area_fraction_col = area_fraction['column']
+area_fraction_rows = area_fraction['rows']
+area_fraction_col = area_fraction['column']
 
-# for cell_row_index in range(area_fraction_rows[0], area_fraction_rows[1] + 1):
-#     area_fraction = ws[area_fraction_col + str(cell_row_index)].value
-#     area_fraction_percentage = area_fraction * 100
+for cell_row_index in range(area_fraction_rows[0], area_fraction_rows[1] + 1):
+    area_fraction = ws[area_fraction_col + str(cell_row_index)].value
+    area_fraction_percentage = area_fraction * 100
 
-#     for color_indicator in color_indicators:
+    color = 'blue'
 
-wb.save(filename)
-os.startfile(file_dir)
+    for color_indicator in color_indicators:
+        color_indicator_range = color_indicators[color_indicator]
+        color_indicator_lower_range = color_indicator_range[0]
+        color_indicator_upper_range = color_indicator_range[1]
+
+        is_area_fraction_percentage_in_range = area_fraction_percentage >= color_indicator_lower_range and area_fraction_percentage <= color_indicator_upper_range
+
+        if is_area_fraction_percentage_in_range:
+            color = color_indicator
+            break
+
+    print(cell_row_index)
+    print(color)
+
+# wb.save(filename)
+# os.startfile(file_dir)
